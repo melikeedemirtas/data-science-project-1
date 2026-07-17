85e1bc2275f184412157640fa3dcec5372601a96
Melike <melike@Melike-MacBook-Air.local>
2026-07-17 22:37:34 +0300

    feat: solve SQL questions for project 1

--- data/questions.py
+++ data/questions.py
@@ -16,17 +16,19 @@ def connect_db():
 def question_1_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT customer_name, country FROM customers;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
     return data
 
 
+
+
 def question_2_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT * FROM orders ORDER BY total_amount DESC LIMIT 5;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -36,7 +38,7 @@ def question_2_query():
 def question_3_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT product_name, price FROM products ORDER BY price ASC LIMIT 3;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -46,7 +48,7 @@ def question_3_query():
 def question_4_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT * FROM customers ORDER BY signup_date ASC LIMIT 10;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -56,7 +58,7 @@ def question_4_query():
 def question_5_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT product_name,stock_quantity FROM products ORDER BY stock_quantity DESC LIMIT 1;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -66,7 +68,7 @@ def question_5_query():
 def question_6_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT * FROM orders ORDER BY order_date DESC LIMIT 1;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -76,7 +78,7 @@ def question_6_query():
 def question_7_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT product_name FROM products ORDER BY product_name ASC;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -86,17 +88,16 @@ def question_7_query():
 def question_8_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT customer_id, email FROM customers ORDER BY customer_id ASC LIMIT 5;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
     return data
 
-
 def question_9_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute('SELECT order_id, total_amount FROM orders ORDER BY total_amount ASC LIMIT 3;')
     data = cursor.fetchall()
     cursor.close()
     connection.close()
@@ -106,7 +107,7 @@ def question_9_query():
 def question_10_query():
     connection = connect_db()
     cursor = connection.cursor()
-    cursor.execute('')
+    cursor.execute("SELECT * FROM customers WHERE country = 'Turkey' ORDER BY customer_name ASC;")
     data = cursor.fetchall()
     cursor.close()
     connection.close()
